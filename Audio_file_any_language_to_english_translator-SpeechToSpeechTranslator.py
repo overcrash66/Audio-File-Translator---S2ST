7fcdf5997366f1843834c9f4a717a6bcc653a570
overcrash <3681221+overcrash66@users.noreply.github.com>
2023-10-15 01:31:02 -0300

    Initial commit

---
+++ Audio_file_any_language_to_english_translator-SpeechToSpeechTranslator.py
@@ -0,0 +1,145 @@
+from tkinter import Tk, Label, Button, filedialog, StringVar, OptionMenu, messagebox, ttk, DoubleVar
+import threading
+import time
+from PIL import Image, ImageTk
+import pygame
+from transformers import WhisperProcessor, WhisperForConditionalGeneration
+from gtts import gTTS
+import torchaudio
+import logging
+
+class CustomTranslator:
+    def __init__(self):
+        self.processor = WhisperProcessor.from_pretrained("openai/whisper-large-v2")
+        self.model = WhisperForConditionalGeneration.from_pretrained("openai/whisper-large-v2")
+
+    def translate_audio(self, input_path, output_path):
+        try:
+            # Load input audio file using torchaudio
+            input_waveform, input_sampling_rate = torchaudio.load(input_path)
+
+            # Ensure the input audio has a proper frame rate
+            if input_sampling_rate != 16000:
+                resampler = torchaudio.transforms.Resample(orig_freq=input_sampling_rate, new_freq=16000)
+                input_waveform = resampler(input_waveform)
+
+            # Process the input audio with the Whisper processor
+            input_features = self.processor(input_waveform.numpy(), sampling_rate=16000, return_tensors="pt")
+
+            # Generate token ids
+            predicted_ids = self.model.generate(input_features["input_features"])
+
+            # Decode token ids to text
+            transcription = self.processor.batch_decode(predicted_ids, skip_special_tokens=True)[0]
+
+            # Save the translated text to an MP3 file
+            tts = gTTS(transcription, lang='en')
+            tts.save(output_path)
+
+            # Log success
+            logging.info(f"Translation successful for {input_path}. Translated text: {transcription}")
+
+        except Exception as e:
+            # Log errors
+            logging.error(f"Error processing audio: {e}")
+            raise  # Re-raise the exception
+            
+    def play_audio(self, audio_path):
+        pygame.mixer.init()
+        pygame.mixer.music.load(audio_path)
+        pygame.mixer.music.play()
+        
+    def stop_audio(self):
+        pygame.mixer.music.stop()
+        
+class TranslatorGUI:
+    def __init__(self, master):
+        master.title("Audio file from any language to english translator - Speech to speech translator")
+        master.geometry("480x640")  # Increased height for progress bar and label
+
+        # Load and display banner image
+        banner_image = Image.open("Flag_of_Palestine.svg.png")
+        banner_image = banner_image.resize((480, 200))
+        banner_photo = ImageTk.PhotoImage(banner_image)
+        banner_label = Label(master, image=banner_photo)
+        banner_label.image = banner_photo
+        banner_label.pack()
+
+        # Create a frame to center everything
+        center_frame = ttk.Frame(master)
+        center_frame.pack(expand=True)
+
+        self.label_input = Label(center_frame, text="Select MP3 File:")
+        self.label_input.pack(side="top", pady=10)
+
+        self.browse_button = Button(center_frame, text="Browse", command=self.browse)
+        self.browse_button.pack(side="top", pady=10)
+        
+        self.label_file_title = Label(center_frame, text="Selected File Title:")
+        self.label_file_title.pack(side="top", pady=5)
+        
+        self.translate_button = Button(center_frame, text="Translate", command=self.translate)
+        self.translate_button.pack(side="top", pady=10)
+
+        self.play_button = Button(center_frame, text="Play Selected File", command=self.play_selected)
+        self.play_button.pack(side="top", pady=5)
+
+        self.stop_button = Button(center_frame, text="Stop playing Selected File", command=self.stop_playing)
+        self.stop_button.pack(side="top", pady=5)
+
+        self.progress_bar = ttk.Progressbar(center_frame, variable=DoubleVar(), mode='indeterminate')
+        self.progress_bar.pack(side="top", pady=5)
+
+        self.label_status = Label(center_frame, text="")
+        self.label_status.pack(side="top", pady=5)
+
+        self.audio_path = ""
+    
+    def browse(self):
+        file_path = filedialog.askopenfilename(filetypes=[("MP3 Files", "*.*")])
+        print(f"Selected file: {file_path}")
+        self.audio_path = file_path
+        
+        # Display file title
+        file_title = file_path.split("/")[-1]  # Extract the file name from the path
+        self.label_file_title.config(text=f"Selected File Title: {file_title}")
+
+    def translate(self):
+        if self.audio_path:
+            output_path = filedialog.asksaveasfilename(defaultextension=".mp3", filetypes=[("MP3 Files", "*.mp3")])
+            if output_path:
+                translation_thread = threading.Thread(target=self.run_translation, args=(output_path,))
+                translation_thread.start()
+                self.progress_bar.start()
+                self.label_status.config(text="Translation in progress...")
+
+    def run_translation(self, output_path):
+        translator_instance = CustomTranslator()
+        translator_instance.translate_audio(self.audio_path, output_path)
+        
+        PlayOutputFile = CustomTranslator()
+        PlayOutputFile.play_audio(output_path)
+        
+        self.progress_bar.stop()
+        self.label_status.config(text="Translation complete!")
+        
+        # Show success message
+        messagebox.showinfo("Success", f"Translation saved successfully at:\n{output_path}")
+        
+    def play_selected(self):
+        if self.audio_path:
+            translator_instance = CustomTranslator()
+            translator_instance.play_audio(self.audio_path)
+
+    def stop_playing(self):
+        translator_instance = CustomTranslator()
+        translator_instance.stop_audio()
+        
+# Initialize pygame mixer
+pygame.mixer.init()
+
+# Example usage
+if __name__ == "__main__":
+    root = Tk()
+    app = TranslatorGUI(root)
+    root.mainloop()
---
+++ readme.md
@@ -0,0 +1,57 @@
+# MP3 Translator
+
+Audio_file_any_language_to_english_translator-SpeechToSpeechTranslator is a simple tool that allows you to translate the content of an Audio file using OpenAI's Whisper ASR and TTS models.
+
+## Features
+
+- Translate the content of an Audio file from one language to english.
+- Uses OpenAI's Whisper models for audio processing and translation.
+- User-friendly GUI for easy interaction.
+
+## Requirements
+
+Make sure you have the following dependencies installed:
+
+- Python 3.x
+- Pip (Python package installer)
+
+Install the required Python packages using:
+
+```bash
+pip install -r requirements.txt
+```
+
+## Usage
+
+Run the Script:
+
+```bash
+python Audio_file_any_language_to_english_translator-SpeechToSpeechTranslator.py
+```
+
+Select an MP3 File:
+	Click the "Browse" button to choose an MP3 file.
+	You can use 'segment_1.mp3' as example (with a spanish voice)
+
+Translate Speech:
+	Click the "Translate" button to translate the speech to English.
+
+Save Translated Audio:
+	Choose a save location using the "Save" dialog.
+
+
+## GUI Preview
+
+![Audio_file_any_language_to_english_translator-SpeechToSpeechTranslator GUI](Screenshot.png)
+
+## Configuration
+
+- You can customize the translation model and other settings by modifying the script.
+
+## Logging
+
+- The script logs translation results and errors to a log file named log.txt.
+
+## License
+
+This project is licensed under the [MIT License](https://opensource.org/licenses/MIT) - see the [LICENSE](LICENSE) file for details.
